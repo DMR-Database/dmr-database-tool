775b7570535f4cca7b06e3e764d13c1fc87016f0
Albert Einstein <javastraat@users.noreply.github.com>
2024-07-02 18:41:33 +0200

    Update build.sh

--- Docker/build.sh
+++ Docker/build.sh
@@ -1,2 +1,2 @@
 docker image rm dmr-database-tool
-docker build -t dmr-database-tool https://raw.githubusercontent.com/DMR-Database/dmr-database-tool/main/Docker/Dockerfile
+docker build --no-cache -t dmr-database-tool https://raw.githubusercontent.com/DMR-Database/dmr-database-tool/main/Docker/Dockerfile
